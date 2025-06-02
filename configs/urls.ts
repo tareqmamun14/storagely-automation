7207b25bb534c67a0d1bfdb68e0a4e7dc427ac3b
Tareq Mamun <tareqmamun14@gmail.com>
2025-06-03 00:52:42 +0600

    update URL's

--- configs/urls.ts
+++ configs/urls.ts
@@ -30,6 +30,9 @@ export enum Environment {
       'https://test.staging.storagely-api.com/gatekeeper-self-storage/storage-units/georgia/peachtree-city/senoia-road',
       'https://test.staging.storagely-api.com/storage-boss/storage-units/louisiana/ponchatoula/west-pine-street',
       'https://test.staging.storagely-api.com/mini-mall-storage/storage-units/arkansas/batesville/batesville-blvd',
+      'https://test.staging.storagely-api.com/first-storage/storage-units/north-carolina/fayetteville/raeford-road',
+      'https://test.staging.storagely-api.com/allpurposestorage/storage-units/maine/belgrade/manchester-road',
+      
     ],
     [Environment.PRODUCTION]: [
       'https://10federalstorage.com/storage-units/texas/arlington/avenue-f',
@@ -44,27 +47,30 @@ export enum Environment {
       'https://rhino-storage.com/storage-units/louisiana/covington/philip-drive',
       'https://gatekeeperstoragega.com/storage-units/georgia/peachtree-city/senoia-road',
       'https://storagedepotla.com/storage-units/louisiana/ponchatoula/west-pine-street',
-      'https://app.storagely.io/mini-mall-storage/storage-units/arkansas/batesville/batesville-blvd'
-
-
+      'https://app.storagely.io/mini-mall-storage/storage-units/arkansas/batesville/batesville-blvd',
+      'https://app.storagely.io/first-storage/storage-units/north-carolina/fayetteville/raeford-road',
+      'https://app.storagely.io/allpurposestorage/storage-units/maine/belgrade/manchester-road',
     ]
   };
   
   // FMS Platform information
   export const FMS_PLATFORM: Record<string, string> = {
-    'https://10federalstorage.com/storage-units/north-carolina/high-point/greensboro-road': 'Storedge',
-    'https://www.bestboxstorage.com/storage-units/missouri/ofallon/highway-k': 'Storedge',
-    'https://radiantstorage.com/storage-units/texas/galveston/church-street': 'Storedge',
-    'https://yourpremierstorage.com/storage-units/mississippi/laurel/ms-15': 'Storedge',
-    'https://redrocksstorage.com/storage-units/colorado/aurora/east-14th-avenue': 'Storedge',
-    'https://distinctstorage.com/storage-units/connecticut/new-milford/kent-road': 'Storedge',
-    'https://www.storagestar.com/storage-units/colorado/colorado-springs/aerotech-drive': 'SSM',
-    'https://bluebirdstorage.ca/storage-units/alberta/calgary/blackfoot-trail': 'Sitelink',
-    'https://sunbirdstorage.com/storage-units/nc/winston-salem/country-club-road': 'Sitelink',
-    'https://rhino-storage.com/storage-units/louisiana/covington/philip-drive': 'Sitelink',
-    'https://gatekeeperstoragega.com/storage-units/georgia/peachtree-city/senoia-road': 'Sitelink',
-    'https://storagedepotla.com/storage-units/louisiana/ponchatoula/west-pine-street': 'Sitelink',
-    'https://app.storagely.io/mini-mall-storage/storage-units/alabama/courtland/highway-33': 'Stitelink',
+      'https://10federalstorage.com/storage-units/north-carolina/high-point/greensboro-road': 'storEDGE',
+      'https://www.bestboxstorage.com/storage-units/missouri/ofallon/highway-k': 'storEDGE',
+      'https://radiantstorage.com/storage-units/texas/galveston/church-street': 'storEDGE',
+      'https://yourpremierstorage.com/storage-units/mississippi/laurel/ms-15': 'storEDGE',
+      'https://redrocksstorage.com/storage-units/colorado/aurora/east-14th-avenue': 'storEDGE',
+      'https://distinctstorage.com/storage-units/connecticut/new-milford/kent-road': 'storEDGE',
+      'https://www.storagestar.com/storage-units/colorado/colorado-springs/aerotech-drive': 'SSM',
+      'https://bluebirdstorage.ca/storage-units/alberta/calgary/blackfoot-trail': 'SiteLink',
+      'https://sunbirdstorage.com/storage-units/nc/winston-salem/country-club-road': 'SiteLink',
+      'https://rhino-storage.com/storage-units/louisiana/covington/philip-drive': 'SiteLink',
+      'https://gatekeeperstoragega.com/storage-units/georgia/peachtree-city/senoia-road': 'SiteLink',
+      'https://storagedepotla.com/storage-units/louisiana/ponchatoula/west-pine-street': 'SiteLink',
+      'https://app.storagely.io/mini-mall-storage/storage-units/alabama/courtland/highway-33': 'SiteLink',
+      'https://app.storagely.io/first-storage/storage-units/north-carolina/fayetteville/raeford-road' : 'storEDGE',
+      'https://app.storagely.io/allpurposestorage/storage-units/maine/belgrade/manchester-road' : 'storEDGE',
+
   };
   
   // Helper function to get the current set of URLs based on environment
